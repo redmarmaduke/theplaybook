14cd5cff38025a1fc8ab049318c94d9a508bfd23
Manuel Nunes <17111297+redmarmaduke@users.noreply.github.com>
2020-01-24 17:03:16 -0800

    remove test code from routes.js

--- controllers/routes.js
+++ controllers/routes.js
@@ -2,9 +2,6 @@ const db = require('../models');
 
 module.exports = function(app) {
     app.get("/", function(request,response) {
-        db.User.findAll().then(function(reason) {
-            console.log(reason);
-        });
         response.render('hello');
     });
 
